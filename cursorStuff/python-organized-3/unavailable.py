9f3be9f201146850b9cc35e34fe3c8e8d2aabd67
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-12-12 16:41:10 -0500

    Truck Plus BS, Python

--- cursorStuff/python-organized-3/unavailable.py
+++ cursorStuff/python-organized-3/unavailable.py
@@ -183,9 +183,9 @@ my_list = {
 'TITLE', 'TIZZY', 'TOAST', 'TODAY', 'TONIC', 'TOOTH', 'TOPAZ', 'TOPIC', 'TORCH', 'TORSO', 
 'TOTAL', 'TOTEM', 'TOUCH', 'TOUGH', 'TOWEL', 'TOWER', 'TOXIC', 'TOXIN', 'TRACE', 'TRACK', 
 'TRACT', 'TRADE', 'TRAIL', 'TRAIN', 'TRAIT', 'TRASH', 'TRAWL', 'TREAT', 'TREND', 'TRIAD', 
-'TRICE', 'TRICK', 'TRIPE', 'TRITE', 'TROLL', 'TROOP', 'TROPE', 'TROUT', 'TROVE', 'TRULY', 
-'TRUSS', 'TRUST', 'TRUTH', 'TRYST', 'TUBER', 'TULIP','TUNIC', 'TURBO', 'TUTOR', 'TWANG', 
-'TWEAK', 
+'TRICE', 'TRICK', 'TRIPE', 'TRITE', 'TROLL', 'TROOP', 'TROPE', 'TROUT', 'TROVE', 'TRUCK', 
+'TRULY', 'TRUSS', 'TRUST', 'TRUTH', 'TRYST', 'TUBER', 'TULIP', 'TUNIC',  'TURBO', 'TUTOR', 
+'TWANG', 'TWEAK', 
 'TWEED', 'TWICE', 'TWINE', 'TWIRL', 'UDDER', 'ULCER', 'ULTRA', 'UNCLE', 'UNDER', 'UNDID', 
 'UNDUE', 'UNFED', 'UNFIT', 'UNIFY', 'UNION', 'UNITE', 'UNLIT', 'UNMET', 'UNTIE', 'UNTIL', 
 'UNZIP', 'UPPER', 'UPSET', 'URBAN', 'USAGE', 'USHER', 'USING', 'USUAL', 'USURP', 'UTTER', 
@@ -201,7 +201,7 @@ my_list = {
 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY'
 }
 
-word_to_find = 'GUESS'
+word_to_find = 'TRUCK'
 
 
 if word_to_find in my_list:
