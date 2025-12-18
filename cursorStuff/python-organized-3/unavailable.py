99be89873787b5af24734b1441cee94f263d20b4
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-12-18 00:39:03 -0500

    Rugby Plus BS, Python Style

--- cursorStuff/python-organized-3/unavailable.py
+++ cursorStuff/python-organized-3/unavailable.py
@@ -145,9 +145,9 @@ my_list = {
 'RIGHT', 'RIGID', 'RIPER', 
 'RISEN', 'RIVAL', 'RIVET', 'ROACH', 'ROBIN', 'ROBOT', 'ROCKY', 'RODEO', 'ROGUE', 'ROUGE', 
 'ROOMY', 'ROUGH', 'ROUND', 'ROUSE', 'ROUTE', 'ROVER', 'ROWDY', 'ROWER', 'ROYAL', 'RUDDY', 
-'RUDER', 'RUMBA', 'RUPEE', 'RUSTY', 'SAINT', 'SALAD', 'SALLY', 'SALSA', 'SALTY', 'SANDY', 
-'SASSY', 'SAUCY', 'SAUNA', 'SAUTE', 'SAVOR', 'SAVVY', 'SCALD', 'SCALE', 'SCANT', 'SCARE', 
-'SCARF', 'SCENT', 'SCOFF', 
+'RUDER', 'RUGBY', 'RUMBA', 'RUPEE', 'RUSTY', 'SAINT', 'SALAD', 'SALLY', 'SALSA', 'SALTY', 
+'SANDY', 'SASSY', 'SAUCY', 'SAUNA', 'SAUTE', 'SAVOR', 'SAVVY', 'SCALD', 'SCALE', 'SCANT', 
+'SCARE', 'SCARF', 'SCENT', 'SCOFF', 
 'SCOLD', 'SCONE', 'SCOPE', 'SCORE', 'SCORN', 'SCOUR', 'SCOUT', 'SCOWL', 'SCRAM', 'SCRAP', 
 'SCRUB', 'SCRUM', 'SEDAN', 'SEEDY', 'SEGUE', 'SENSE', 'SERUM', 'SERVE', 'SEVEN', 'SEVER', 
 'SHADE', 
@@ -204,7 +204,7 @@ my_list = {
 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY'
 }
 
-word_to_find = 'GRASS'
+word_to_find = 'RUGBY'
 
 
 if word_to_find in my_list:
