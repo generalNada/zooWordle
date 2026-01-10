c0388881e41b61cf85eb6e93915c8d953f7738b5
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-01-09 21:10:39 -0500

    Eight Dealt With, Python Style

--- cursorStuff/python-organized-3/unavailable.py
+++ cursorStuff/python-organized-3/unavailable.py
@@ -56,7 +56,8 @@ my_list = {
 'DOUGH', 'DOWEL', 'DOWRY', 'DOZEN', 'DRAFT', 'DRAIN', 'DRAPE', 'DRAWN', 'DREAD', 'DREAM', 
 'DRILL', 'DRINK', 'DRIVE', 'DROLL', 'DRONE', 'DROOL', 'DROOP', 'DROVE', 'DRYER', 'DUCHY', 
 'DUMMY', 'DUTCH', 'DUVET', 'DWARF', 'DWELL', 'DWELT', 'EAGER', 'EAGLE', 'EARLY', 'EARTH', 
-'EASEL', 'EBONY', 'EDIFY', 'EGRET', 'EJECT', 'ELATE', 'ELBOW', 'ELDER', 'ELITE', 'ELOPE', 
+'EASEL', 'EBONY', 'EDIFY', 'EGRET', 'EIGHT', 'EJECT', 'ELATE', 'ELBOW', 'ELDER', 'ELITE', 
+'ELOPE', 
 'ELUDE', 'EMAIL', 'EMBER', 'EMPTY', 'ENACT', 'ENDOW', 'ENEMA', 'ENJOY', 'ENNUI', 'ENSUE', 
 'ENTER', 'EPOCH', 'EPOXY', 'EQUAL', 'EQUIP', 'ERASE', 'ERODE', 'ERROR', 'ERUPT', 'ESSAY', 
 'ETHER', 
@@ -214,7 +215,7 @@ my_list = {
 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY'
 }
 
-word_to_find = 'BLAST'
+word_to_find = 'EIGHT'
 
 
 if word_to_find in my_list:
