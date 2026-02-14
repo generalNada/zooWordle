2e934f832fc5d5a495ac7e2a1e531363e87b7d77
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-02-14 17:20:54 -0500

    Bloom Dealt With, Python Style

--- cursorStuff/python-organized-3/unavailable.py
+++ cursorStuff/python-organized-3/unavailable.py
@@ -24,7 +24,8 @@ my_list = {
 'BEVEL', 'BICEP', 'BILGE', 'BINGE', 'BIOME', 'BIRCH', 'BIRTH', 'BLACK', 'BLADE', 'BLAME', 
 'BLAND', 'BLANK', 'BLARE', 'BLAST', 'BLAZE', 'BLEAK', 'BLEAT', 'BLEED', 'BLEEP', 'BLEND', 
 'BLIMP', 'BLINK', 
-'BLISS', 'BLOCK', 'BLOKE', 'BLOND', 'BLOWN', 'BLUFF', 'BLUNT', 'BLURB', 'BLURT', 'BLUSH', 
+'BLISS', 'BLOCK', 'BLOKE', 'BLOND', 'BLOOM', 'BLOWN', 'BLUFF', 'BLUNT', 'BLURB', 'BLURT', 
+'BLUSH', 
 'BOARD', 'BOAST', 'BONGO', 'BONUS', 'BOOBY', 'BOOST', 'BOOTY', 'BOOZE', 'BOOZY', 'BORAX', 
 'BORNE', 'BOSSY', 'BOUGH', 'BOXER', 'BRACE', 'BRAID', 'BRAIN', 'BRAKE', 'BRAND', 'BRASH', 
 'BRASS', 'BRAVE', 'BRAVO', 'BRAWN', 'BREAD', 'BREAK', 'BREED', 'BRIAR', 
@@ -236,7 +237,7 @@ my_list = {
 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY'
 }
 
-word_to_find = 'MOOCH'
+word_to_find = 'BLOOM'
 
 
 if word_to_find in my_list:
