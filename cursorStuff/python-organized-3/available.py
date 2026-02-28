24264d9ad72bd7eaa8b56237b936157429e08436
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-02-27 22:27:55 -0500

    Dizzy Dealt With, Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -25,7 +25,7 @@ unused_list = [
             "CRUMP", "CURRY", "CURVY", "CUTIE", "CYCLE", 
             "DAILY", "DAIRY", "DEALT", "DEBAR", 
             "DEIGN", "DEMON", "DEMUR", "DENSE", "DERBY", "DILLY", "DIMLY", 
-            "DIODE", "DIRGE", "DIRTY", "DITCH", "DIVAN", "DIVER", "DIZZY",
+            "DIODE", "DIRGE", "DIRTY", "DITCH", "DIVAN", "DIVER", 
             "DOWDY", "DOWNY", "DRAKE", "DRAMA", "DRANK", "DRAWL", 
             "DRESS", "DRIED", "DRIER", "DRIFT", "DROIT", "DROSS", 
             "DROWN", "DRUID", "DRUNK", "DRYLY", "DULLY", "DUMPY", "DUNCE", 
@@ -124,7 +124,7 @@ unused_list = [
             "WREST", "WRING", "WRYLY", "ZONAL" 
              ]
 
-word = 'LANCE'
+word = 'DIZZY'
 
 
 if word in unused_list:
