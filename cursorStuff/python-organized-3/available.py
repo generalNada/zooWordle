c6ec3b3e8552914148df65fd4626aab704067dd8
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-03-22 22:20:00 -0400

    Basil Dealt With, Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -2,7 +2,7 @@ unused_list = [
     "ABLED", "ABODE", "ABORT", "ABUSE", "ADORN", "AFIRE", "AFOOT", "AFOUL", "AIDER", "ALGAE",
     "ALIBI", "ALIGN", "ALLAY", "ALLEY", "ALLOY", "AMAZE", "AMEND", "ANIME", 
     "ANNUL", "APING", "APNEA", "ARENA", "ARMOR", "AROSE", "ARRAY", "ARSON", "ASTIR", "AUGUR",
-    "AUNTY", "AVIAN", "AXIAL", "AXION", "BAGGY", "BASAL", "BASIL", "BASIS", "BEECH", "BEFIT",
+    "AUNTY", "AVIAN", "AXIAL", "AXION", "BAGGY", "BASAL", "BASIS", "BEECH", "BEFIT",
     "BEGAN", "BEGAT", "BEGUN", "BELLE", "BERRY", "BETEL", "BEVEL", "BEZEL", "BIBLE", "BIDDY",
     "BIGOT", "BILLY", "BINGO", "BISON", "BITTY", "BLESS", "BLIND", "BLITZ", "BLOAT", "BLOOD",
     "BLUER", "BOBBY", "BONEY", "BOOTH", "BOSOM", "BOTCH", "BOULE", "BOUND", "BOWEL", "BRAWL",
@@ -68,7 +68,7 @@ unused_list = [
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'SLICK'
+word = 'BASIL'
 
 
 if word in unused_list:
