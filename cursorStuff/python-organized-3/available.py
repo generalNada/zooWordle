1c2fa78583340cbfce9a9f23843420b3eea1310e
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-04-09 23:32:52 -0400

    Laden Dealt With, Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -28,7 +28,7 @@ unused_list = [
     "HARPY", "HARRY", "HAUTE", "HEDGE", "HIPPY", "HITCH", "HONOR", "HOOCH", "HORNY",
     "HOTLY", "HUMUS", "HUSKY", "HUSSY", "HYDRO", "HYMEN", "ICILY", "IDIOT", "IDYLL", "ILIAC",
     "IMPLY", "INGOT", "INTRO", "ISSUE", "JETTY", "JOIST", "JUICY", "JUNTA", "JUNTO",
-    "JUROR", "KAPPA", "KINKY", "KITTY", "KNEED", "LADEN", "LATCH", "LEANT", "LEGAL", "LEPER",
+    "JUROR", "KAPPA", "KINKY", "KITTY", "KNEED", "LATCH", "LEANT", "LEGAL", "LEPER",
     "LIEGE", "LIKEN", "LIPID", "LOAMY", "LOATH", "LOGIN", "LOOSE", "LORRY", "LUMEN",
     "LUPUS", "LURCH", "LYMPH", "LYRIC", "MACRO", "MAFIA", "MAMMA", "MAMMY", "MANGE", "MANGY",
     "MEATY", "MECCA", "MELEE", "MILKY", "MINIM", "MINOR", "MISSY", "MOCHA", "MOODY", "MORON",
@@ -69,7 +69,7 @@ unused_list = [
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'INLET'
+word = 'LADEN'
 
 
 if word in unused_list:
