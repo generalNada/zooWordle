fe8fad6290fd5a7bc2076c3cb00a527fb4dadc3c
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-04-22 23:11:44 -0400

    Snore Dealt With, Python Style

--- cursorStuff/python-organized-3/unavailable.py
+++ cursorStuff/python-organized-3/unavailable.py
@@ -168,8 +168,8 @@ my_list = {
     'SLOTH', 'SLUMP', 'SLUNG', 'SMALL', 'SMART', 'SMASH', 'SMEAR', 'SMELL', 'SMELT', 'SMILE', 
     'SMIRK',
     'SMITE', 'SMITH', 'SMOCK', 'SMOKE', 'SNACK', 'SNAFU', 'SNAIL', 'SNAKE', 'SNAKY', 'SNARE',
-    'SNARL', 'SNEAK', 'SNIDE', 'SNOOP', 'SNORT', 'SNOUT', 'SOBER', 'SOGGY', 'SOLAR', 'SOLID', 
-    'SOLVE',
+    'SNARL', 'SNEAK', 'SNIDE', 'SNOOP', 'SNORE', 'SNORT', 'SNOUT', 'SOBER', 'SOGGY', 'SOLAR', 
+    'SOLID', 'SOLVE',
     'SONIC', 'SORRY', 'SOUND', 'SOUTH', 'SOWER', 'SPACE', 'SPADE', 'SPARE', 'SPASM', 'SPARK',
     'SPATE', 'SPEAK', 'SPEAR', 'SPECK', 'SPEED', 'SPELL', 'SPELT', 'SPEND', 'SPENT', 'SPICE',
     'SPICY', 'SPIEL', 'SPIKE', 'SPILL', 'SPINE', 'SPINY', 'SPIRE', 'SPITE', 'SPLAT', 'SPLIT',
@@ -217,7 +217,7 @@ my_list = {
     'WRUNG', 'YACHT', 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY'
 }
 
-word_to_find = 'CLUMP'
+word_to_find = 'SNORE'
 
 
 if word_to_find in my_list:
