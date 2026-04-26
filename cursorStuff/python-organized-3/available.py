bde44d1d93e6738653e8da1662e10e17f7378da8
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-04-25 23:33:50 -0400

    Women Dealt With, Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -64,11 +64,11 @@ unused_list = [
     "URINE", "UTILE", "VALOR", "VALVE", "VAPOR", "VAUNT", "VERSE", "VERSO", "VICAR", "VIGIL",
     "VIPER", "VIRUS", "VISIT", "VISTA", "VOCAL", "VOMIT", "WAGER", "WAIVE", "WARTY",
     "WAVER", "WELCH", "WELSH", "WHARF", "WIDER", "WIDOW", "WIGHT", "WILLY", "WIMPY",
-    "WINCH", "WISPY", "WOMEN", "WOODY", "WOOLY", "WOOZY", "WORTH", "WRACK", "WRECK",
+    "WINCH", "WISPY", "WOODY", "WOOLY", "WOOZY", "WORTH", "WRACK", "WRECK",
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'DRUNK'
+word = 'WOMEN'
 
 
 if word in unused_list:
