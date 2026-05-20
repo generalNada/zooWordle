3328e6ee85bb3ff64eb18729e0c5f506a9773df7
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-05-19 23:36:28 -0400

    Dusty Dealt With, Python Style
    Changed the word from 'LOATH' to 'DUSTY' in available.py.

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -16,7 +16,7 @@ unused_list = [
     "DAIRY", "DEALT", "DEBAR", "DEIGN", "DEMON", "DEMUR", "DERBY", "DILLY", "DIMLY",
     "DIODE", "DIRGE", "DIRTY", "DITCH", "DIVAN", "DIVER", "DOWNY", "DRAKE", 
     "DRANK", "DRAWL", "DRESS", "DRIED", "DRIER", "DRIFT", "DROIT", "DROSS", "DROWN", "DRUID",
-    "DRYLY", "DULLY", "DUMPY", "DUNCE", "DUSTY", "DYING", "EATER", "ECLAT",
+    "DRYLY", "DULLY", "DUMPY", "DUNCE", "DYING", "EATER", "ECLAT",
     "EDICT", "EKING", "ELECT", "ELEGY", "ELIDE", "EMCEE", "ENEMY", "ENTRY",
     "ERECT", "ESTER", "ETUDE", "EVICT", "EXPEL", "EYING", "FAIRY", "FANNY", "FATAL",
     "FATTY", "FAUNA", "FECAL", "FELLA", "FELON", "FEMME", "FEMUR", "FENCE", "FETAL", "FETUS",
@@ -68,7 +68,7 @@ unused_list = [
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'LOATH'
+word = 'DUSTY'
 
 
 if word in unused_list:
