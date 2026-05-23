8c90c8931918b9e63cff2557c4be142f0ce47991
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-05-22 23:26:48 -0400

    Vocal Dealt With, Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -62,13 +62,13 @@ unused_list = [
     "TORUS", "TRAMP", "TREAD", "TRIBE", "TRIED", "TRUCE", "TRUER", "TRUMP", "TRUNK", "TUBAL",
     "TULLE", "TUMOR", "TWIXT", "TYING", "UNCUT", "UNITY", "UNSET", "UNWED",
     "URINE", "UTILE", "VALOR", "VALVE", "VAPOR", "VAUNT", "VERSE", "VERSO", "VICAR", "VIGIL",
-    "VIPER", "VIRUS", "VISIT", "VISTA", "VOCAL", "VOMIT", "WAGER", "WAIVE", "WARTY",
+    "VIPER", "VIRUS", "VISIT", "VISTA", "VOMIT", "WAGER", "WAIVE", "WARTY",
     "WELCH", "WELSH", "WHARF", "WIDER", "WIDOW", "WIGHT", "WILLY", "WIMPY",
     "WINCH", "WISPY", "WOODY", "WOOLY", "WOOZY", "WORTH", "WRACK",
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'AGREE'
+word = 'VOCAL'
 
 
 if word in unused_list:
