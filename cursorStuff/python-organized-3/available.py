0a06d5dc99c9108dc6b07b64ab68d9aa501ecabe
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-06-16 23:19:32 -0400

    Amaze Dealt With, Python Style
    Changed the word variable from 'BROIL' to 'AMAZE'.

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -1,6 +1,6 @@
 unused_list = [
     "ABLED", "ABODE", "ABORT", "ABUSE", "ADORN", "AFIRE", "AFOUL", "AIDER", "ALGAE",
-    "ALIBI", "ALLAY", "AMAZE", "AMEND", "ANIME",
+    "ALIBI", "ALLAY", "AMEND", "ANIME",
     "ANNUL", "APING", "APNEA", "ARENA", "ARMOR", "AROSE", "ARRAY", "ARSON", "ASTIR", "AUGUR",
     "AUNTY", "AVIAN", "AXIAL", "AXION", "BAGGY", "BASAL", "BEECH",
     "BEGAN", "BEGAT", "BERRY", "BETEL", "BEVEL", "BEZEL", "BIBLE", "BIDDY",
@@ -68,7 +68,7 @@ unused_list = [
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'BROIL'
+word = 'AMAZE'
 
 
 if word in unused_list:
