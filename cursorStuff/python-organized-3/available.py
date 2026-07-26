bf3aa6a83eca2e65803e005661d35e942ca48d73
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-07-26 14:39:28 -0400

    Grape Dealt With, Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -23,7 +23,7 @@ unused_list = [
     "FICUS", "FIGHT", "FILER", "FILMY", "FILTH", "FLACK", "FLECK", "FLEET", "FLIER",
     "FLUID", "FLUSH", "FRAUD", "FREED", "FREER", "FRIAR", "FRISK", "FUDGE", "FUROR", "FURRY",
     "FUSSY", "GABLE", "GAFFE", "GAILY", "GASSY", "GATOR", "GAYER", "GAYLY", "GAZER", "GEEKY",
-    "GIPSY", "GIRLY", "GIVER", "GODLY", "GOLLY", "GONAD", "GOURD", "GRAPE", "GRAZE",
+    "GIPSY", "GIRLY", "GIVER", "GODLY", "GOLLY", "GONAD", "GOURD", "GRAZE",
     "GRILL", "GROPE", "GROWN", "GRUNT", "GUILT", "GULCH", "GUSTO", "GUTSY", "GYPSY", "HAREM",
     "HARPY", "HARRY", "HAUTE", "HEDGE", "HIPPY", "HITCH", "HONOR", "HOOCH", "HORNY",
     "HOTLY", "HUMUS", "HUSKY", "HUSSY", "HYDRO", "HYMEN", "ICILY", "IDIOT", "IDYLL", "ILIAC",
@@ -67,7 +67,7 @@ unused_list = [
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'ALOHA'
+word = 'GRAPE'
 
 
 if word in unused_list:
