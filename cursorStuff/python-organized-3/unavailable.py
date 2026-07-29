cdfa0fb1b2a1ae19cd096de5090e64f0e864b32d
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-07-28 21:49:42 -0400

    Sonar Dealt With, Python Style

--- cursorStuff/python-organized-3/unavailable.py
+++ cursorStuff/python-organized-3/unavailable.py
@@ -205,7 +205,7 @@ my_list = {
     'SMILE', 'SMIRK',
     'SMITE', 'SMITH', 'SMOCK', 'SMOKE', 'SNACK', 'SNAFU', 'SNAIL', 'SNAKE', 'SNAKY', 'SNARE',
     'SNARL', 'SNEAK', 'SNIDE', 'SNOOP', 'SNORE', 'SNORT', 'SNOUT', 'SOBER', 'SOGGY', 'SOLAR', 
-    'SOLID', 'SOLVE',
+    'SOLID', 'SOLVE', 'SONAR',
     'SONIC', 'SORRY', 'SOUND', 'SOUTH', 'SOWER', 'SPACE', 'SPADE', 'SPARE', 'SPASM', 'SPARK',
     'SPATE', 'SPEAK', 'SPEAR', 'SPECK', 'SPEED', 'SPELL', 'SPELT', 'SPEND', 'SPENT', 'SPICE',
     'SPICY', 'SPIEL', 'SPIKE', 'SPILL', 'SPINE', 'SPINY', 'SPIRE', 'SPITE', 'SPLAT', 'SPLIT',
@@ -262,7 +262,7 @@ my_list = {
     'WROTE', 'WRUNG', 'YACHT', 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY'
 }
 
-word_to_find = 'POSER'
+word_to_find = 'SONAR'
 
 
 if word_to_find in my_list:
