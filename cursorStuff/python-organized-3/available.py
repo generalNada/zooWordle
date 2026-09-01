3609a0ab46b36097819afd2af78412ee85c6efdc
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-08-31 23:57:57 -0400

    Mound Dealt With , Python Style

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -32,7 +32,7 @@ unused_list = [
     "LIEGE", "LIPID", "LOAMY", "LOGIN", "LUMEN",
     "LUPUS", "LURCH", "LYMPH", "LYRIC", "MACRO", "MAMMA", "MAMMY", "MANGE", "MANGY",
     "MEATY", "MECCA", "MELEE", "MILKY", "MINIM", "MINOR", "MISSY", "MOCHA", "MOODY", "MORON",
-    "MOUND", "MOWER", "MUCUS", "MUDDY", "MUSKY", "NACHO",
+    "MOWER", "MUCUS", "MUDDY", "MUSKY", "NACHO",
     "NEWER", "NINNY", "NOOSE", "NOSEY", "NUTTY", "OAKEN",
     "OASES", "OBESE", "OCTAL", "ODDER", "OMBRE", "OPIUM", "OPTIC", "OUTGO", 
     "OVARY", "OVINE", "OVOID", "OWING", "PADDY", "PAGAN", "PALER", "PALSY", "PANSY",
