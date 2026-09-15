62f6cfac06c7e47850c182e2c3b77243c584d503
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-09-14 23:04:31 -0400

    Rinse Dealt With, Python Style
    Changed the word to check for availability from 'FOCUS' to 'RINSE'.

--- cursorStuff/python-organized-3/available.py
+++ cursorStuff/python-organized-3/available.py
@@ -42,7 +42,7 @@ unused_list = [
     "PUNCH", "PUREE", "PURER", "PUSHY", "PYGMY", "QUASI",
     "QUOTH", "RABBI", "RADAR", "RADII", "RAJAH", "RALLY", "RALPH", "RANDY",
     "RARER", "RASPY", "RAVEN", "RAZOR", "REARM", "REBAR", "RECUT", "REEDY", "REFIT", 
-    "REIGN", "RENAL", "RESET", "RIFLE", "RIGOR", "RINSE", "RIPEN", "RISKY",
+    "REIGN", "RENAL", "RESET", "RIFLE", "RIGOR", "RIPEN", "RISKY",
     "RIVER", "ROAST", "ROGER", "ROTOR", "RUMOR", "SADLY", "SAFER", "SALON",
     "SALVE", "SALVO", "SANER", "SAPPY", "SATYR", "SAUCE", "SAVOY", "SCALP", "SCALY",
     "SCAMP", "SCARY", "SCION", "SCREE", "SCREW", "SCROD", "SCUBA", "SEIZE", "SEMEN", 
@@ -67,7 +67,7 @@ unused_list = [
     "WREST", "WRING", "WRYLY", "ZONAL"
 ]
 
-word = 'FOCUS'
+word = 'RINSE'
 
 
 if word in unused_list:
