9dffc2ad34daf9521bf72fa3c7d9210e8e879c85
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-09-16 22:57:08 -0400

    Wispy Plus BS

--- mediaQueryHaiku/script.js
+++ mediaQueryHaiku/script.js
@@ -14,7 +14,7 @@ if (bwToggle) {
       bwToggle.textContent = "🎨 Color / B&W";
     }
 
-    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF");
+    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, Jakdick");
   });
 } else {
   console.error("B&W toggle button not found!");
