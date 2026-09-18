214dddc1417ae58e7bd9fc453bcb7aec56086071
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-09-17 22:05:21 -0400

    Theta Plus BS

--- mediaQueryHaiku/script.js
+++ mediaQueryHaiku/script.js
@@ -14,7 +14,7 @@ if (bwToggle) {
       bwToggle.textContent = "🎨 Color / B&W";
     }
 
-    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, Jakdick");
+    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, JakOffdick");
   });
 } else {
   console.error("B&W toggle button not found!");
