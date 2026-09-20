eb7a5046653a396595057a7e9f44524d110cb98e
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-09-20 19:49:06 -0400

    Shone Plus BS

--- mediaQueryHaiku/script.js
+++ mediaQueryHaiku/script.js
@@ -14,7 +14,7 @@ if (bwToggle) {
       bwToggle.textContent = "🎨 Color / B&W";
     }
 
-    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, WakenDick");
+    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, SHONEDick");
   });
 } else {
   console.error("B&W toggle button not found!");
