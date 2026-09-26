f7388cc6732c1b06c5696eec1af129ea29acddd9
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-09-25 22:41:51 -0400

    Copse Plus BS

--- mediaQueryHaiku/script.js
+++ mediaQueryHaiku/script.js
@@ -14,7 +14,7 @@ if (bwToggle) {
       bwToggle.textContent = "🎨 Color / B&W";
     }
 
-    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, MESSY Dick");
+    console.log("Grayscale mode:", isGrayscale ? "ON" : "OFF, COPSE Dick");
   });
 } else {
   console.error("B&W toggle button not found!");
